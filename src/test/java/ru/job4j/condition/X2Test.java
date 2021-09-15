9bcd9e61d084f970674aae76f060297f5dcdc3e7
whitevax <whitevax@gmail.com>
2021-09-15 22:00:50 +0300

    1.2. Входные данные в тесте [#297750]

---
+++ src/test/java/ru/job4j/condition/X2Test.java
@@ -0,0 +1,54 @@
+package ru.job4j.condition;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class X2Test {
+
+    @Test
+    public void whenA10B0C0X2Then40() {
+        int a = 10;
+        int b = 0;
+        int c = 0;
+        int x = 2;
+        int expected = 40;
+        int rsl = X2.calc(a, b, c, x);
+        Assert.assertEquals(expected, rsl);
+    }
+
+    @Test
+    public void firstTest() {
+        int a = 1;
+        int b = 1;
+        int c = 1;
+        int x = 1;
+        int expected = 3;
+        int rsl = X2.calc(a, b, c, x);
+        Assert.assertEquals(expected, rsl);
+        a = 0;
+        b = 1;
+        c = 1;
+        x = 1;
+        expected = 2;
+        rsl = X2.calc(a, b, c, x);
+        Assert.assertEquals(expected, rsl);
+    }
+
+    @Test
+    public void second() {
+        int a = 1;
+        int b = 1;
+        int c = 0;
+        int x = 1;
+        int expected = 2;
+        int rsl = X2.calc(a, b, c, x);
+        Assert.assertEquals(expected, rsl);
+        a = 1;
+        b = 1;
+        c = 1;
+        x = 0;
+        expected = 1;
+        rsl = X2.calc(a, b, c, x);
+        Assert.assertEquals(expected, rsl);
+    }
+}
