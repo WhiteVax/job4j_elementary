6f07a4df97a2f8be71465634227c2729b34b5855
whitevax <whitevax@gmail.com>
2021-10-10 21:00:02 +0300

    6.0.2. Заполнение массива. [#226300 #214522]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -13,5 +13,9 @@ public class ArrayDefinition {
         names[1] = "Dmitry";
         names[2] = "Sergey";
         names[3] = "Artemy";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
