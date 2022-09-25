f2f882680c4318bae378bdd18acf08ea5e70a0f6
whitevax <whitevax@gmail.com>
2022-09-25 19:19:56 +0300

    6.9. String - это массив [#504884 #346131]

--- src/main/java/ru/job4j/array/JavaNameValidator.java
+++ src/main/java/ru/job4j/array/JavaNameValidator.java
@@ -1,30 +1,32 @@
 package ru.job4j.array;
 
 public class JavaNameValidator {
-        public static boolean isNameValid(String name) {
-            char[] chars = name.toCharArray();
-            var isSpecial = false;
-            var isUpper = false;
-            var isLower = false;
-            if (chars.length != 0) {
-                isLower = isLowerLatinLetter(chars[0]);
-                for (var el : chars) {
-                    isUpper = isUpperLatinLetter(el);
-                    isSpecial = isSpecialSymbol(el);
-                }
+    public static boolean isNameValid(String name) {
+        var rsl = true;
+        if (name.isEmpty() || !isLowerLatinLetter(name.charAt(0))) {
+            return false;
+        }
+
+        for (int i = 1; i < name.length(); i++) {
+            var in = name.codePointAt(i);
+            if (!(isLowerLatinLetter(in) || isUpperLatinLetter(in)
+                    || isSpecialSymbol(in) || Character.isDigit(in))) {
+                rsl = false;
+                break;
             }
-            return isLower || isUpper || isSpecial;
         }
+        return rsl;
+    }
 
-        public static boolean isSpecialSymbol(int code) {
-            return code == 35 || code == 95;
-        }
+    public static boolean isSpecialSymbol(int code) {
+        return code == 36 || code == 95;
+    }
 
-        public static boolean isUpperLatinLetter(int code) {
-            return code >= 65 && code <= 95;
-        }
+    public static boolean isUpperLatinLetter(int code) {
+        return code >= 65 && code <= 95;
+    }
 
-        public static boolean isLowerLatinLetter(int code) {
-            return code >= 97 && code <= 122;
-        }
+    public static boolean isLowerLatinLetter(int code) {
+        return code >= 97 && code <= 122;
+    }
 }
